a3c294790b9eb21d985f5556154bbf070e78aaa9
Ramos <1joaoramos1@gmail.com>
2024-04-16 15:25:52 +0100

    Tests

---
+++ test/pt/up/fe/comp/ramos_tests/SemanticAnalysisTest.java
@@ -0,0 +1,34 @@
+package pt.up.fe.comp.ramos_tests;
+
+import org.junit.Test;
+import pt.up.fe.comp.TestUtils;
+import pt.up.fe.specs.util.SpecsIo;
+
+public class SemanticAnalysisTest {
+    @Test
+    public void StaticalMethodAttributeAccess() {
+        var result = TestUtils.analyse(SpecsIo.getResource(
+                "pt/up/fe/comp/ramos_tests/StaticalMethodAttributeAccess.jmm"
+        ));
+        System.out.println(result.getReports());
+        TestUtils.mustFail(result);
+    }
+
+    @Test
+    public void StaticalClassAttributeAccess() {
+        var result = TestUtils.analyse(SpecsIo.getResource(
+                "pt/up/fe/comp/ramos_tests/StaticalClassAttributeAccess.jmm"
+        ));
+        System.out.println(result.getReports());
+        TestUtils.mustFail(result);
+    }
+
+    @Test
+    public void ImportedClassAttributeAccess() {
+        var result = TestUtils.analyse(SpecsIo.getResource(
+                "pt/up/fe/comp/ramos_tests/ImportedClassAttributeAccess.jmm"
+        ));
+        System.out.println(result.getReports());
+        TestUtils.noErrors(result);
+    }
+}
