6559814fd3d06eb0f1ee86c0faa04f12872ad0aa
Ramos <1joaoramos1@gmail.com>
2024-04-18 11:30:50 +0100

    No test

--- test/pt/up/fe/comp/ramos_tests/RamosTests.java
+++ test/pt/up/fe/comp/ramos_tests/RamosTests.java
@@ -27,37 +27,4 @@ public class RamosTests {
         System.out.println(result.getReports());
         TestUtils.mustFail(result);
     }
-
-    @Test
-    public void compileArithmetic() {
-        testJmmCompilation("pt/up/fe/comp/ramos_tests/ReturnBinaryOpWithField.jmm", this::compileArithmetic);
-    }
-
-    public void compileArithmetic(ClassUnit classUnit) {
-        // Test name of the class
-        assertEquals("Class name not what was expected", "CompileArithmetic", classUnit.getClassName());
-
-        // Test foo
-        var methodName = "foo";
-        Method methodFoo = classUnit.getMethods().stream()
-                .filter(method -> method.getMethodName().equals(methodName))
-                .findFirst()
-                .orElse(null);
-
-        assertNotNull("Could not find method " + methodName, methodFoo);
-
-        var binOpInst = methodFoo.getInstructions().stream()
-                .filter(inst -> inst instanceof AssignInstruction)
-                .map(instr -> (AssignInstruction) instr)
-                .filter(assign -> assign.getRhs() instanceof BinaryOpInstruction)
-                .findFirst();
-
-        assertTrue("Could not find a binary op instruction in method " + methodName, binOpInst.isPresent());
-
-        var retInst = methodFoo.getInstructions().stream()
-                .filter(inst -> inst instanceof ReturnInstruction)
-                .findFirst();
-        assertTrue("Could not find a return instruction in method " + methodName, retInst.isPresent());
-
-    }
 }
